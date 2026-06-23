67778718337c770484465d7c173b560f2f01a4b6
Abhay-333 <abhayd0331@gmail.com>
2026-06-23 20:21:29 +0530

    feat(auth): create error classes

---
+++ backend/src/utils/Errors/Errors.js
@@ -0,0 +1,36 @@
+import { StatusCodes } from "http-status-codes";
+
+export class AppError extends Error {
+  constructor(
+    message,
+    statusCode = StatusCodes.INTERNAL_SERVER_ERROR,
+    details = null,
+  ) {
+    super(message, statusCode, details);
+    this.name = this.constructor.name;
+    this.statusCode = statusCode;
+    this.details = details;
+    Error.captureStackTrace?.(this, this.constructor);
+  }
+}
+
+export class UnauthorizeError extends Error {
+  constructor(message = "Unauthorize user", details = null) {
+    super(message, StatusCodes.UNAUTHORIZED, details);
+    Error.captureStackTrace?.(this, this.constructor);
+  }
+}
+
+export class BadRequestError extends Error {
+  constructor(message = "Bad Request", details = null) {
+    super(message, StatusCodes.BAD_REQUEST, details);
+    Error.captureStackTrace?.(this, this.constructor);
+  }
+}
+
+export class NotFoundError extends Error {
+  constructor(message = "Resource not found", details = null) {
+    super(message, StatusCodes.NOT_FOUND, details);
+    Error.captureStackTrace?.(this, this.constructor);
+  }
+}
--- backend/src/utils/Errors/NotFound.js
+++
@@ -1,9 +0,0 @@
-import { StatusCodes } from "http-status-codes";
-
-export default class NotFoundError extends Error {
-  constructor(message = "Resource not found") {
-    super(message);
-    this.statusCode = StatusCodes.NOT_FOUND;
-    Error.captureStackTrace?.(this, this.constructor);
-  }
-}
