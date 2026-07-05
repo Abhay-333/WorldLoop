a718620c534409aaa3ba0250ca3e4a1d5c056526
Abhay-333 <abhayd0331@gmail.com>
2026-07-05 17:36:44 +0530

    feat(auth): create and implement googleOAuthMiddleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import env from "./config/env.js";
 import indexRouter from "./routes/index.route.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import securityMiddleware from "./middlewares/security.middlware.js";
+import googleOAuthMiddleware from "./middlewares/googleOAuth.middleware.js";
 
 const createServer = () => {
   const app = express();
@@ -11,11 +12,10 @@ const createServer = () => {
   if (env.NODE_ENV === "development") {
     app.use(morgan(env.MORGAN_LOGGER));
   }
-  
+
   securityMiddleware(app);
-
+  googleOAuthMiddleware(app);
   app.use("/api/v1", indexRouter);
-
 
   // Error middleware should be used after all the routes
   app.use(errorMiddleware);
--- backend/src/middlewares/googleOAuth.middleware.js
+++ backend/src/middlewares/googleOAuth.middleware.js
@@ -1,3 +1,18 @@
-const googleOAuthMiddleware = (req, res, next) => {
-    
+import passport, { session } from "passport";
+import env from "../config/env.js";
+
+const googleOAuthMiddleware = (app) => {
+  return async (req, res, err) => {
+    app.use(
+      session({
+        secret: env.SESSION_SECRET,
+        resave: false, //Ye session ko tabhi save karta hai jab usme koi change aaya ho.
+        saveUninitialized: false, //Ye nayi aur empty session ko bhi force karke save karta hai, bhale hi user ne login na kiya ho ya session mein kuch store na kiya ho
+      }),
+    );
+    app.use(passport.initialize());
+    app.use(passport.session());
+  };
 };
+
+export default googleOAuthMiddleware;
