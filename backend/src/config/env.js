d2bcb0e472c8a0d102f1e63f86ac246fbcdbf8fc
Abhay-333 <abhayd0331@gmail.com>
2026-07-05 17:23:18 +0530

    feat(auth): config express-session and updated the env

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -30,6 +30,7 @@ const envSchema = z.object({
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
   GOOGLE_CALLBACK_URL: z.string(),
+  SESSION_SECRET: z.string(),
 });
 
 const parsed = envSchema.safeParse(process.env);
---
+++ backend/src/middlewares/googleOAuth.middleware.js
@@ -0,0 +1,3 @@
+const googleOAuthMiddleware = (req, res, next) => {
+    
+};
