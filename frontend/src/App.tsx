9c1c1bbb208369d6a28209e1645c2e4774ad9901
Abhay-333 <abhayd0331@gmail.com>
2026-07-07 12:33:47 +0530

    feat(ui): create input label separator components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button"
+import LoginPage from "@/features/auth/login/LoginPage.jsx" 
 
 export function App() {
   return (
     <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
+      {/* <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
         <div>
           <h1 className="font-medium">Project ready!</h1>
           <p>You may now add components and start building.</p>
@@ -13,7 +14,9 @@ export function App() {
         <div className="font-mono text-xs text-muted-foreground">
           (Press <kbd>d</kbd> to toggle dark mode)
         </div>
-      </div>
+      </div> */}
+
+      <LoginPage></LoginPage>
     </div>
   )
 }
--- frontend/src/features/auth/login/LoginCard.jsx
+++ frontend/src/features/auth/login/LoginCard.jsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
+import { Card, CardContent, CardFooter, CardHeader } from "../../../components/ui/card"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
