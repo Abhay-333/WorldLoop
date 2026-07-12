e9332104469346122a04ad31fe38f21f106ad0a1
Abhay-333 <abhayd0331@gmail.com>
2026-07-12 15:12:36 +0530

    feat(ui): fix redux theme state

--- frontend/src/features/theme/ThemeButton.jsx
+++ frontend/src/features/theme/ThemeButton.jsx
@@ -1,6 +1,6 @@
 // src/shared/components/mode-toggle.tsx
 import { Moon, Sun } from "lucide-react"
-
+import { useEffect } from "react"
 import { useTheme } from "../../app/theme-provider"
 import { Button } from "../../components/ui/button"
 import {
@@ -9,27 +9,29 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "../../components/ui/dropdown-menu"
+import { useDispatch, useSelector } from "react-redux"
+import { setTheme } from "./themeSlice"
 
 function ThemeButton() {
-  const { setTheme } = useTheme()
+  const dispatch = useDispatch()
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 bg-zinc-400 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 bg-zinc-400 transition-all dark:scale-100 dark:rotate-0" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
+        <DropdownMenuItem onClick={() => dispatch(setTheme("light"))}>
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
+        <DropdownMenuItem onClick={() => dispatch(setTheme("dark"))}>
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
+        <DropdownMenuItem onClick={() => dispatch(setTheme("system"))}>
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
