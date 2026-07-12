e023ed6b382b6d5a74e764216f9991769729ea1a
Abhay-333 <abhayd0331@gmail.com>
2026-07-12 15:12:36 +0530

    feat(ui): fix redux theme state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import RegisterPage from "./features/auth/register/RegisterPage"
 import PublicRoutes from "./app/router/PublicRoutes"
 import HomeFeed from "./features/home/HomeFeed"
 import PrivateRoutes from "./app/router/PrivateRoutes"
-import ThemeButton from "./features/theme/ThemeButton"
 const routes = [
   {
     path: "/",
@@ -18,7 +17,7 @@ const routes = [
   {
     path: "/home",
     element: <PrivateRoutes />,
-    children: [{ path: "", element: <ThemeButton /> }],
+    children: [{ path: "", element: <HomeFeed /> }],
   },
 ]
 
