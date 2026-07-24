4295ec647be0ca53add2194049536f33f1a0636e
Abhay-333 <abhayd0331@gmail.com>
2026-07-24 10:22:42 +0530

    feat(routes): implement loading and error handling in PrivateRoutes

--- frontend/src/app/router/PrivateRoutes.jsx
+++ frontend/src/app/router/PrivateRoutes.jsx
@@ -1,7 +1,12 @@
 import React from "react"
-import { Outlet } from "react-router"
+import { Outlet, Navigate } from "react-router"
+import useMe from "../hooks/useMe"
 
 const PrivateRoutes = () => {
+  const { data, isPending, isError } = useMe()
+
+  if (isPending) return <div>Loading...</div>
+  if (isError || !data?.success) return <Navigate to="/" replace />
   return <Outlet />
 }
 
