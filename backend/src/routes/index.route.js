cb06b227d526677a3a9ee422c8ba492a7762cc95
Abhay-333 <abhayd0331@gmail.com>
2026-07-31 12:48:57 +0530

    feat(user): remove password reset fields from user schema and update user routes

--- backend/src/routes/index.route.js
+++ backend/src/routes/index.route.js
@@ -1,8 +1,10 @@
 import express from "express";
 import authRouter from "../modules/auth/auth.route.js";
+import userRouter from ""
 
 const indexRouter = express.Router();
 
 indexRouter.use("/auth", authRouter);
 
+indexRouter.use("/user", userRouter);
 export default indexRouter;
---
+++ backend/src/scripts/seedUsers.js
@@ -0,0 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import mongoose from "mongoose";
+import { faker } from "@faker-js/faker";
+import UserModel from "../models/user.model";
