e6b93ccb7fec7ad9ebeeb770b509cf0540b6a99d
Abhay-333 <abhayd0331@gmail.com>
2026-08-03 13:37:56 +0530

    fix(imports): update import paths for user controller and service to include file extensions

--- backend/src/modules/user/user.controller.js
+++ backend/src/modules/user/user.controller.js
@@ -1,3 +1,5 @@
+import UserService from "./user.service.js";
+
 export default class UserController {
   constructor() {
     this.userService = new UserService();
--- backend/src/routes/index.route.js
+++ backend/src/routes/index.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import authRouter from "../modules/auth/auth.route.js";
-import userRouter from ""
+import userRouter from "../modules/user/user.route.js";
 
 const indexRouter = express.Router();
 indexRouter.use("/auth", authRouter);
