8cd7645efc0fab08e23beb5161b61b261f3bd9e4
Abhay-333 <abhayd0331@gmail.com>
2026-08-03 13:09:31 +0530

    fix(routes): correct user route import and instantiate UserController

--- backend/src/routes/index.route.js
+++ backend/src/routes/index.route.js
@@ -3,7 +3,6 @@ import authRouter from "../modules/auth/auth.route.js";
 import userRouter from ""
 
 const indexRouter = express.Router();
-
 indexRouter.use("/auth", authRouter);
 
 indexRouter.use("/user", userRouter);
