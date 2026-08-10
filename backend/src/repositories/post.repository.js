9d3b4bcd3f34371e41c3df0e88d510da3cf2d481
Abhay-333 <abhayd0331@gmail.com>
2026-08-10 12:58:54 +0530

    feat(posts): implement PostsRepository with findByAuthorId method for retrieving posts by author

---
+++ backend/src/repositories/post.repository.js
@@ -0,0 +1,12 @@
+import PostModel from "../models/post.model";
+
+export default class PostsRepository {
+  async findByAuthorId(userId) {
+    return await PostModel.find({
+      author: userId,
+      isArchived: false,
+    })
+      .sort({ createdAt: -1 })
+      .populate("author", "username fullName avatar");
+  }
+}
