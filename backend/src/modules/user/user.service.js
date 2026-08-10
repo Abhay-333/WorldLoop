be45df6f6741090753ec217531aac18e82c1a386
Abhay-333 <abhayd0331@gmail.com>
2026-08-10 12:01:58 +0530

    feat(user): implement update profile functionality and enhance user posts retrieval

--- backend/src/modules/user/user.service.js
+++ backend/src/modules/user/user.service.js
@@ -1,5 +1,8 @@
 import UserRepo from "../../repositories/user.repository.js";
-import { BadRequestError } from "../../utils/Errors/app-errors.js";
+import {
+  BadRequestError,
+  NotFoundError,
+} from "../../utils/Errors/app-errors.js";
 
 export default class UserService {
   constructor() {
@@ -8,14 +11,29 @@ export default class UserService {
 
   async getUserProfileService(username) {
     if (!username) throw BadRequestError("Username is Required.");
-
     const user = await this.userRepo.findOne({ username });
+    return user;
+  }
+
+  async updateProfileService(userId, profileData) {
+    const allowedFields = ["displayName", "bio", "website", "location"];
+    const updates = {};
+
+    for (const field of allowedFields) {
+      if (profileData[field] !== undefined) {
+        updates[field] = profileData[field];
+      }
+    }
+
+    const user = await this.userRepo.updateById(userId);
     console.log(user);
+
+    if (!user) throw new NotFoundError("User not found.");
 
     return user;
   }
 
-  async updateProfileService(username) {
+  async getUserPostsService(username) {
     if (!username) throw BadRequestError("Username is Required.");
 
     const user = await this.userRepo.findOne({ username });
