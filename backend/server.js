6c66400e5a2733ffc4da0ac6b33f02e82d79d5a0
Abhay-333 <abhayd0331@gmail.com>
2026-08-14 12:33:01 +0530

    fix(server): refactor PORT variable usage for consistency in server startup

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,11 @@ import env from "./src/config/env.js";
 import logger from "./src/config/logger.js";
 
 const app = createServer();
-
+const PORT = env.PORT || 5000;
 connectDb()
   .then(() => {
-    app.listen(env.PORT, () => {
-      logger.info(`Server running on ${env.PORT}`);
+    app.listen(PORT, () => {
+      logger.info(`Server running on ${PORT}`);
     });
   })
   .catch((err) => {
