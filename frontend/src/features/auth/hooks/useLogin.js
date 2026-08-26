531aa7ff63a87c4a2d6c56ec984836af72f2905b
Abhay-333 <abhayd0331@gmail.com>
2026-08-26 13:52:21 +0530

    refactor(auth): consolidate login and registration logic into setCredentials

--- frontend/src/features/auth/hooks/useLogin.js
+++ frontend/src/features/auth/hooks/useLogin.js
@@ -2,14 +2,20 @@ import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { useNavigate } from "react-router"
 import { loginApi } from "../api/auth.api"
 import toast from "react-hot-toast"
+import { useDispatch } from "react-redux"
+import { setCredentials } from "../authSlice"
 
 const useLogin = () => {
   const navigate = useNavigate()
   const queryClient = useQueryClient()
+  const dispatch = useDispatch()
+
   return useMutation({
     mutationFn: loginApi,
     onSuccess: (data) => {
-      queryClient.setQueryData(["auth", "me"], data.data.user)
+      const { user, accessToken } = data.data
+      queryClient.setQueryData(["me"], { ...data, data: user })
+      dispatch(setCredentials({ user, token: accessToken }))
       navigate("/home")
     },
     onError: (error) => {
